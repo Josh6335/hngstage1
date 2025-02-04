d4ee667578e81ed0f03ba7dc70754d7399fe46c2
Musediku Joshua <musedikutemitope@gmail.com>
2025-02-04 05:28:34 +0100

    file updated

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-route::get('/classifynumber', [ClassifyNumberController::class, 'classifyNumber']);
+route::get('/classify-number', [ClassifyNumberController::class, 'classifyNumber']);
